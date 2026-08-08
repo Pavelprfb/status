ebc0bdc5463a9147a11b175e069e3803a6d7a43f
Pavelprfb <pavelprfb@gmail.com>
2026-08-08 22:12:31 +0600

    layout: add Yandex.Metrika counter script

--- app/layout.js
+++ app/layout.js
@@ -93,6 +93,15 @@ const preloadScript = `(function () {
   } catch (e) {}
 })();`;
 
+const yandexMetrikaScript = `(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111421060', 'ym');
+
+ym(111421060, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`;
+
 const jsonLd = {
   "@context": "https://schema.org",
   "@type": "WebSite",
@@ -121,6 +130,16 @@ export default async function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script dangerouslySetInnerHTML={{ __html: yandexMetrikaScript }} />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/111421060"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <PreferencesProvider initialLang={initialLang}>{children}</PreferencesProvider>
       </body>
     </html>
